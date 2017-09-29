2161f60a9ec7d8f14d19ad80ceaf5ba47276579d
Jared Duncan <JDuncan5876@gmail.com>
2017-09-29 01:25:38 -0400

    Fixed up some issues with code

--- ultrasound-cnn/train_ultrasound_cnn.py
+++ ultrasound-cnn/train_ultrasound_cnn.py
@@ -1,5 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
 from keras.layers import Flatten
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
@@ -17,13 +18,13 @@ for imagehandle in [item for item in os.listdir('.') if item.endswith('png')]:
     arr = numpy.asarray(data)
     images.append(numpy.reshape(arr, (1, 360, 360)).astype('float32') / 255)
 
-train_test_pivot = int(0.7 * len(images))
+train_test_separator = int(0.7 * len(images))
 
-x_train = images[:train_test_pivot]
-x_test = images[train_test_pivot:]
+x_train = images[:train_test_separator]
+x_test = images[train_test_separator:]
 
-y_train = images[:train_test_pivot]
-y_test = images[train_test_pivot:]
+y_train = images[:train_test_separator]
+y_test = images[train_test_separator:]
 
 labels = []
 with open('data.txt') as labelfile:
@@ -32,13 +33,13 @@ with open('data.txt') as labelfile:
     label = []
     for strlabel in strlabels:
         label.append(int(strlabel[0]))
-    labels.append[label]
+    labels.append(label)
 
 labels = np_utils.to_categorical(labels)
 num_classes = labels.shape[1]
 def baseline_model():
     model = Sequential()
-    model.add(Conv2D(32, (10, 10), input_shape = (1, 360, 360), activation='relu'))
+    model.add(Conv2D(32, (20, 20), input_shape=(1, 360, 360), activation='relu'))
     model.add(MaxPooling2D(pool_size=(4, 4)))
     model.add(Dropout(0.2))
     model.add(Conv2D(16, (5, 5), activation='relu'))
@@ -49,4 +50,4 @@ def baseline_model():
     model.add(Dense(256, activation='relu'))
     model.add(Dense(num_classes, activation='softmax'))
 model = baseline_model()
-model.fit(images, labels, validation_data=(x_test, y_test) epochs=10, batch_size=200, verbose=2)
+model.fit(images, labels, validation_data=(x_test, y_test), epochs=10, batch_size=200, verbose=2)
