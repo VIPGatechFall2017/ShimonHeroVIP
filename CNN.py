cd21e714ad235110001cbb7f7ff30b735a168ef1
kmorris72 <kipp.morris@gmail.com>
2017-12-12 14:01:39 -0500

    updated data processing script to be more efficient and work better with data, changed CNN model to be more accurate, and included Jared's data preparation script

--- CNN.py
+++ CNN.py
@@ -1,17 +1,50 @@
-import numpy as np
 from data_processing import create_dataset
-from sklearn.metrics import accuracy_score
-#
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+
 # constants
-directory_name = "image_data"
-num_imgs = 300
-input_shape = (360, 360, 3)
-resize_shape = (28, 28)
+directory_name = "image_data/1/hand-images"
+file_name = "frame" # whatever text comes before the number in the names of the
+                    # image files (assumes files are jpg)
+num_imgs = 3600
+input_shape = (740, 600, 3)
+resize_shape = (40, 40)
+crop_amounts = (45, 0, 60, 0) # left, top, right, bottom
 test_data_filename = "data.txt"
-k = 2
-predictions = []
+num_classes = 6
 
 
-(X_train, X_test, y_train, y_test) = create_dataset(directory_name, num_imgs,
-                                                    input_shape, resize_shape,
-test_data_filename)
+(images, test_data) = create_dataset(directory_name, file_name,
+                                                    num_imgs, input_shape,
+                                                    resize_shape, crop_amounts,
+                                                    test_data_filename)
+
+images = images.reshape((num_imgs, resize_shape[0], resize_shape[1], 1))
+
+def build_model():
+    model = Sequential()
+    model.add(Conv2D(16, (5, 5), input_shape=(40, 40, 1),  activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(.1))
+    model.add(Conv2D(8, (4, 4), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(.1))
+    model.add(Conv2D(4, (3, 3), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(.1))
+    model.add(Flatten())
+    model.add(Dense(64, activation="relu"))
+    model.add(Dense(32, activation="relu"))
+    model.add(Dense(num_classes, activation="softmax"))
+    model.compile(loss="categorical_crossentropy", optimizer="adam",
+                  metrics=["accuracy"])
+    return model
+
+model = build_model()
+
+model.fit(images, test_data, validation_split=0.2, epochs=200, batch_size=200,
+          verbose=2)
--- data_processing.py
+++ data_processing.py
@@ -3,54 +3,40 @@ from scipy.misc import imread
 from skimage.color import rgb2grey
 from skimage.transform import resize
 
-#
+
 # pass the name of the folder containing the image files into this function
 # along with the number of images as an integer, the images' 3D shape as a
 # tuple, the desired image shape after conversion to grayscale and downsampling
 # and the name of the file containing the raw data to get a tuple of the
 # format (X_train, X_test, y_train, y_test)
-def create_dataset(directory_name, num_imgs, input_shape, resize_shape,
-                   crop_amounts, test_data_filename):
-    dataset = load_and_process_data(directory_name, num_imgs, input_shape,
-                                    resize_shape, crop_amounts)
+def create_dataset(directory_name, file_name, num_imgs, input_shape,
+                   resize_shape, crop_amounts, test_data_filename):
+    dataset = load_and_process_data(directory_name, file_name, num_imgs,
+                                    input_shape, resize_shape, crop_amounts)
     test_data = load_and_process_test_data(test_data_filename)
     return make_training_and_testing_sets(dataset, num_imgs, test_data)
 
 
-def load_and_process_data(directory_name, num_imgs, input_shape, resize_shape,
-                          crop_amounts):
+def load_and_process_data(directory_name, file_name, num_imgs, input_shape,
+                          resize_shape, crop_amounts):
     # create an empty array with an appropriate 4D shape for the input images
-    dataset_shape = (num_imgs,) + input_shape
+    dataset_shape = (num_imgs,) + resize_shape
     dataset = np.zeros(shape=dataset_shape)
 
     # iterate over files in the image_data folder and load them, skipping
     # files that aren't images
     for i in range(0, num_imgs):
-        dataset[i] = imread("%s/frame%s.jpg" % (directory_name, i))
+        image = imread("%s/%s%s.jpg" % (directory_name, file_name, i))
 
-    # scale each entry in the dataset by 255 to get each value between 0 and 1
-    dataset = dataset / 255
+        # crop the extra black space off of the sides of the image
+        image = crop(image, crop_amounts[0], crop_amounts[1], crop_amounts[2],
+                     crop_amounts[3])
 
-    # crop the extra black space off of the sides of the images
-    new_dataset = np.zeros(shape=(num_imgs,) + (input_shape[0],
-                                                input_shape[1]
-                                                - crop_amounts[0]
-                                                - crop_amounts[2],
-                                                input_shape[2]))
-    for i in range(0, num_imgs):
-        new_dataset[i] = crop(dataset[i], 45, 0, 60, 0)
+        # downsample the image and convert to grayscale
+        image = convert_to_grayscale_and_downsample(image, resize_shape)
 
-    dataset = new_dataset
-
-    # determine the new shape of the dataset in preparation for downsampling
-    # the images, iterate over the images, converting to grayscale and
-    # downsampling, and then replace the old dataset with the new one
-    new_shape = (num_imgs,) + resize_shape
-    new_dataset = np.zeros(new_shape)
-    for i in range(0, num_imgs):
-        new_dataset[i] = convert_to_grayscale_and_downsample(dataset[i],
-                                                             resize_shape)
-    dataset = new_dataset
+        # add the image to the dataset
+        dataset[i] = image
 
     return dataset
 
@@ -67,54 +53,60 @@ def load_and_process_test_data(test_data_filename):
     for line in file:
         processed_line = line.split(",")[1].strip(" ;\n").split(" ")
 
+        finger_open = False
         for i in range(len(processed_line)):
             processed_line[i] = 1 if int(processed_line[i]) > 0.5 else 0
+            if processed_line[i] == 1:
+                finger_open = True
 
+        # adds sixth spot to represent hand with all fingers closed and sets it
+        # appropriately
+        open_hand = [0]
+        if not finger_open:
+            open_hand = [1]
+        processed_line = processed_line + open_hand
         test_data.append(processed_line)
+
+    test_data = np.asarray(test_data)
 
     return test_data
 
 
 def convert_to_grayscale_and_downsample(image, resize_shape):
-    # convert to grayscale
     image = rgb2grey(image)
-
-    # downsample image to 28x28
     image = resize(image, resize_shape, preserve_range=True)
-
     return image
 
 
 def crop(image, left_off, top_off, right_off, bottom_off):
     image = image[top_off:image.shape[0] - bottom_off,
-            left_off:image.shape[1] - right_off,]
+            left_off:image.shape[1] - right_off, ]
     return image
 
 
 def make_training_and_testing_sets(dataset, num_imgs, test_data):
-    # produce arrays made up of approx. 80% of the input and approx. 20% of the
-    # input, respectively
-    training_size = int(np.floor(num_imgs * .8))
-    X_train = np.array(dataset[0:training_size])
-    X_test = np.array(dataset[training_size:len(dataset)])
+    # shuffle the training and testing sets in unison
+    images_shuffled = np.zeros(dataset.shape)
+    test_data_shuffled = np.zeros(test_data.shape)
+    indices = range(num_imgs)
+    np.random.shuffle(indices)
+    for i in indices:
+        images_shuffled[i] = dataset[i]
+        test_data_shuffled[i] = test_data[i]
+    return (images_shuffled, test_data_shuffled)
 
-    # produce arrays containing the data from the text file using length and
-    # dimensions of the input arrays
-    y_train = np.array(test_data[0:training_size])
-    y_test = np.array(test_data[training_size:len(dataset)])
-
-    return (X_train, X_test, y_train, y_test)
 
 # for testing
 directory_name = "image_data/1/hand-images"
+file_name = "frame"  # whatever text comes before the number in the names of the
+# image files (assumes files are jpg)
 num_imgs = 5
 input_shape = (740, 600, 3)
-resize_shape = (28, 28)
-crop_amounts = (45, 0, 60, 0) # left, top, right, bottom
+resize_shape = (40, 40)
+crop_amounts = (45, 0, 60, 0)  # left, top, right, bottom
 test_data_filename = "data.txt"
 
-
-(X_train, X_test, y_train, y_test) = create_dataset(directory_name, num_imgs,
-                                                    input_shape, resize_shape,
-                                                    crop_amounts,
+(images, test_data) = create_dataset(directory_name, file_name,
+                                                    num_imgs, input_shape,
+                                                    resize_shape, crop_amounts,
                                                     test_data_filename)
